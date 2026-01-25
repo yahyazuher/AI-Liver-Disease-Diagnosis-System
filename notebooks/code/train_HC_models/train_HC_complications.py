7e84f0fb9478b8f4ca379ee07831e265e08de576
Yahya Zuher <142307513+yahyazuher@users.noreply.github.com>
2026-01-25 12:24:25 +0300

    Update train_HC_complications.py

--- notebooks/code/train_HC_models/train_HC_complications.py
+++ notebooks/code/train_HC_models/train_HC_complications.py
@@ -13,7 +13,7 @@ Arabic: Google Colab هذا الكود مخصص ومجهز للعمل مباشر
 Created by: Yahya Zuher
 Project: AI-Liver-Diseases-Diagnosis-System
 
-        - Target Output: 0.0 - 1.0 Presentage
+        - Target Output: 0.0 - 1.0 Presentage or int(0,1)
 """
 
 import pandas as pd
